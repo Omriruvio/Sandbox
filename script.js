f0157caab1c5c57cb32e4ab8848a0f9cee62b7c5
Omri Ruvio <omriruvio@gmail.com>
2022-01-21 21:41:43 +0200

    ''

--- script.js
+++ script.js
@@ -676,6 +676,40 @@ decodeMorse = function(morseCode){
   return result.join('').trim();
 }
 
+// 1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246. Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681. The sum of these squares is 84100 which is 290 * 290.
+
+// Task
+// Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+
+// We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+
+// Example:
+// list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+// list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+
+function listSquared(m, n) {
+  const isNumberSquare = (num) => Number.isInteger(Math.sqrt(num));
+  // function that gets an array all of a number's divisors
+  const getDivisorArray = (num) => {
+    let divisorsArray = [];
+    for (let i = 1; i<=num; i++) {
+      if (num % i == 0) divisorsArray.push(i);
+    }
+    return divisorsArray;
+  }
+  const getDivisorSquaredSum = (num) => getDivisorArray(num).map(x => x*x).reduce((x, y) => x+y);
+  const results = [];
+  for (let i = m; i <= n; i++) {
+    const divisorSquaredSum = getDivisorSquaredSum(i);
+    if (isNumberSquare(divisorSquaredSum)) results.push([i, divisorSquaredSum]);
+  }
+  return results;
+}
+
+
+
+
+
 
 
 
