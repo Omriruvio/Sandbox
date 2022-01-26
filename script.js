be5d7c431520b1bbd6a1662b93cb56f2fcbc6e75
Omri Ruvio <omriruvio@gmail.com>
2022-01-27 01:49:53 +0200

    ''

--- script.js
+++ script.js
@@ -836,6 +836,30 @@ function pillars(num_pill, dist, width) {
 }
 
 
+// Given two integers a and b, which can be positive or negative, 
+// find the sum of all the integers between
+//  and including them and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+function getSum( a,b )
+  {
+  if (a == b) {
+    return a; 
+  }
+
+  let low = a < b ? a : b;
+  const high = b > a ? b : a;
+
+  let sum = 0;
+  // loop from low until including high low <= high
+  for (let i = low; low <= high; low++) {
+    sum += low;
+  }
+
+  return sum;
+}
+
+
 
 
 
