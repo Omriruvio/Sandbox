fac376ab2669dff69a5b0f4e4891402cfcb10e43
Omri Ruvio <omriruvio@gmail.com>
2022-01-26 23:49:31 +0200

    ''

--- script.js
+++ script.js
@@ -831,6 +831,10 @@ function Accumulator(startingValue) {
 
 }
 
+function pillars(num_pill, dist, width) {
+  return num_pill == 1 ? 0 : ((num_pill - 1) * dist * 100 + (num_pill - 2) * width);
+}
+
 
 
 
